a7307245fcfe81e837532efc42142dc1a53831a5
Bwis <wisdomEzekiel9@gmail.com>
2026-05-24 11:48:16 +0100

    fix admin controller and prisma config

--- backend-food-website/src/controllers/adminController.js
+++ backend-food-website/src/controllers/adminController.js
@@ -1,3 +1,5 @@
+import { prisma } from '../config/db.js';
+
 const getConfirmedOrder = async (req, res) => {
   try {
     const userId = req.user.id;
@@ -9,16 +11,15 @@ const getConfirmedOrder = async (req, res) => {
       },
     });
 
-    if (!user) {
+    if (!user)
       return res.status(403).json({
         error: "not authorized user",
       });
-    }
 
     const order = await prisma.order.findMany({
       where: {
         status: {
-          in: ["CONFIRMED", "SUCCESSFUL"],
+          OR: ["CONFIRMED", "SUCCESSFUL"],
         },
       },
       orderBy: {
@@ -34,16 +35,61 @@ const getConfirmedOrder = async (req, res) => {
       },
     });
 
-    if (order.length === 0) {
+    if (order.length === 0)
       return res.status(404).json({
         status: "error",
         message: "No orders found",
       });
+
+    res.status(200).json({
+      status: "successful",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+const updateAdminStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (req.user.role !== "ADMIN")
+      return res.status(403).json({
+        error: "not authorized user",
+      });
+
+    const existingOrder = await prisma.order.findUnique({
+      where: { id },
+      include: {
+        orderItem: true,
+      },
+    });
+
+    if (!existingOrder) {
+      return res.status(404).json({ error: "order not found" });
     }
+
+    let totalPrice = 0;
+
+    existingOrder.orderItem.forEach((item) => {
+      totalPrice += item.quantity * item.price;
+    });
+
+    const updateOrder = await prisma.order.update({
+      where: { id },
+      data: {
+        totalPrice,
+        status: "SUCCESSFUL",
+      },
+    });
 
     return res.status(200).json({
       status: "successful",
-      data: order,
+      data: updateOrder,
     });
   } catch (error) {
     return res.status(500).json({
@@ -52,3 +98,5 @@ const getConfirmedOrder = async (req, res) => {
     });
   }
 };
+
+export { getConfirmedOrder, updateAdminStatus };
